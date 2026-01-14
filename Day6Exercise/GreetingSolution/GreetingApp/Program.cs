dc469d8c3fd98bef3dbeed5b1ceb87b83ce8da74
aditya_bhardwaj <973adityabhardwaj@gmail.com>
2026-01-14 15:39:21 +0530

    Day 6 Exercise commited  - Greeting Solution  - Greeting Library  - Greeting App

---
+++ Day6Exercise/GreetingSolution/GreetingApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using GreetingLibrary;
+
+namespace GreetingApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string? name = Console.ReadLine();
+            Console.WriteLine(GreetingHelper.GetGreeting(name));
+        }
+    }
+}
---
+++ Day6Exercise/GreetingSolution/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace GreetingLibrary;
+
+public class GreetingHelper
+{
+    public static string GetGreeting(string? name)
+    {
+        if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+        {
+            name = "Guest";
+        }
+
+        return $"Hello, {name}!";
+    }
+}
