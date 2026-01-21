480845547e086cf22f0b066d9a3512dd4088bfba
aditya_bhardwaj <973adityabhardwaj@gmail.com>
2026-01-21 15:11:00 +0530

    Added Day 14 Exercise

---
+++ Exercises/Day14Exercise/EmployeeClass/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace EmployeeClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new();
+
+            employee.SetId(1);
+            employee.Name = "Aditya Bhardwaj";
+            employee.Department = Departments.IT;
+            employee.Salary = 80000;
+
+            employee.Display();
+        }
+    }
+
+    internal enum Departments
+    {
+        Accounts,
+        Sales,
+        IT
+    }
+
+    internal class Employee
+    {
+        int id;
+
+        public string? Name { get; set; }
+
+        private Departments department;
+        public Departments Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        private int salary;
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 50000 || value <= 90000)
+                {
+                    salary = value;
+                }
+            }
+        }
+
+        public int GetId() { return id; }
+
+        public void SetId(int id) { this.id = id; }
+
+        public void Display()
+        {
+            Console.WriteLine(
+                $"{"ID",-11}: {id}\n" +
+                $"{"NAME",-11}: {Name}\n" +
+                $"{"DEPARTMENT",-11}: {Department}\n" +
+                $"{"SALARY",-11}: {Salary}"
+            );
+        }
+    }
+}
