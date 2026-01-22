1df07d1ca5acee40dc25644bce98a6763dbe91f8
aditya_bhardwaj <973adityabhardwaj@gmail.com>
2026-01-22 11:49:51 +0530

    Added Day 15 Exercise

---
+++ Exercises/Day15Exercise/Day15Exercise/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Day15Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Height person1 = new(5, 6.5m);
+            Height person2 = new(5, 7.5m);
+
+            Console.WriteLine(person1.AddHeights(person2).ToString());
+        }
+    }
+
+    class Height
+    {
+        public int Feet { get; set; }
+        public decimal Inches { get; set; }
+
+        public Height()
+        {
+            Feet = 0;
+            Inches = 0;
+        }
+
+        public Height(int feet, decimal inches)
+        {
+            Feet = feet + ((int) (inches / 12));
+            Inches = inches % 12;
+        }
+
+        public override string ToString()
+        {
+            return $"Height - {Feet} feet {Inches:F1} inches";
+        }
+
+        public Height AddHeights(Height height)
+        {
+            Height tmp = new()
+            {
+                Inches = Inches + height.Inches
+            };
+
+            tmp.Feet = Feet + height.Feet + ((int) tmp.Inches / 12);
+            tmp.Inches %= 12;
+
+            return tmp;
+        }
+    }
+}
