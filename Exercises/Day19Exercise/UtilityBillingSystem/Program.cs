52be164a4dac57c61dc41fd1b5184b5805337254
aditya_bhardwaj <973adityabhardwaj@gmail.com>
2026-01-28 14:26:22 +0530

    Added Day 19 Exercise  - Part 01  - Part 02

---
+++ Exercises/Day19Exercise/UtilityBillingSystem/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace UtilityBillingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UtilityBill> bills = [
+                new ElectricityBill(1, "Aditya", 320, 4),
+                new WaterBill(2, "Aditya", 140, 4),
+                new GasBill(3, "Aditya", 200, 4)
+            ];
+
+            foreach (var bill in bills)
+                bill.PrintBill();
+        }
+    }
+
+    abstract class UtilityBill(
+        int consumerId = 1, 
+        string consumerName = "", 
+        decimal unitsConsumed = 0, 
+        decimal ratePerUnit = 0
+    )
+    {
+        public int ConsumerId { get; set; } = consumerId;
+        public string ConsumerName { get; set; } = consumerName;
+        public decimal UnitsConsumed { get; set; } = unitsConsumed;
+        public decimal RatePerUnit { get; set; } = ratePerUnit;
+
+        public abstract decimal CalculateBillAmount();
+
+        public virtual decimal CalculateTax(decimal billAmount) =>
+            billAmount * 0.05m;
+
+        public void PrintBill()
+        {
+            decimal finalAmount = 
+                CalculateBillAmount() + CalculateTax(UnitsConsumed * RatePerUnit);
+            Console.WriteLine(
+                $"Id: {ConsumerId}, Name: {ConsumerName}, " + 
+                $"Total Units: {UnitsConsumed}, " +
+                $"Final Amount: {finalAmount:N2}"
+            );
+        }
+    }
+
+    class ElectricityBill(
+        int consumerId, 
+        string consumerName, 
+        decimal unitsConsumed, 
+        decimal ratePerUnit
+    ) : UtilityBill(consumerId, consumerName, unitsConsumed, ratePerUnit)
+    {
+        public override decimal CalculateBillAmount() =>
+            (UnitsConsumed > 300) 
+                ? UnitsConsumed * RatePerUnit + (UnitsConsumed * RatePerUnit * 0.10m)
+                : UnitsConsumed * RatePerUnit;
+    }
+
+    class WaterBill(
+        int consumerId, 
+        string consumerName, 
+        decimal unitsConsumed, 
+        decimal ratePerUnit
+    ) : UtilityBill(consumerId, consumerName, unitsConsumed, ratePerUnit)
+    {
+        public override decimal CalculateBillAmount() =>
+            UnitsConsumed * RatePerUnit;
+
+        public override decimal CalculateTax(decimal billAmount) =>
+            billAmount * 0.02m;
+    }
+
+    class GasBill(
+        int consumerId, 
+        string consumerName, 
+        decimal unitsConsumed, 
+        decimal ratePerUnit
+    ) : UtilityBill(consumerId, consumerName, unitsConsumed, ratePerUnit)
+    {
+        public override decimal CalculateBillAmount() =>
+            UnitsConsumed * RatePerUnit + 150;
+
+        public override decimal CalculateTax(decimal billAmount) => 0;
+    }
+}
---
+++ Exercises/Day19Exercise/VehicleSimulation/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace VehicleSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle[] vehicles =
+            [
+                new ElectricCar("Mahindra"),
+                new HeavyTruck("Tata"),
+                new CargoPlane("Air India")
+            ];
+
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                vehicles[i].Move();
+                Console.WriteLine(vehicles[i].GetFuelStatus());
+                Console.WriteLine();
+            }
+        }
+    }
+
+    abstract class Vehicle(string modelName)
+    {
+        public string ModelName { get; } = modelName;
+
+        public abstract void Move();
+
+        public virtual string GetFuelStatus() =>
+            "Fuel level is stable.";
+    }
+
+    class ElectricCar(string modelName) : Vehicle(modelName)
+    {
+        public override void Move() =>
+            Console.WriteLine($"{ModelName} is gliding silently on battery power.");
+
+        public override string GetFuelStatus() =>
+            $"{ModelName} battery is at 80%.";
+    }
+
+    class HeavyTruck(string modelName) : Vehicle(modelName)
+    {
+        public override void Move() =>
+            Console.WriteLine($"{ModelName} is hauling cargo with high-torque diesel power.");
+    }
+
+    class CargoPlane(string modelName) : Vehicle(modelName)
+    {
+        public override void Move() =>
+            Console.WriteLine($"{ModelName} is ascending to 30,000 feet.");
+
+        public override string GetFuelStatus() =>
+            $"{base.GetFuelStatus()} Checking jet fuel reserves...";
+    }
+}
