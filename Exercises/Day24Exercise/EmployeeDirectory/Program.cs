33ced221d8a06f79d0c2346321ca0b9fc0e65279
aditya_bhardwaj <973adityabhardwaj@gmail.com>
2026-02-03 14:30:49 +0530

    Added Day 24 Exercises - Legacy Employee Directory - High Score Leaderboard

---
+++ Exercises/Day24Exercise/EmployeeDirectory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace EmployeeDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = [];
+            hashtable.Add(101, "Alice");
+            hashtable.Add(102, "Bob");
+            hashtable.Add(103, "Charlie");
+            hashtable.Add(104, "Diana");
+
+            if (hashtable[105] != null)
+                Console.WriteLine("ID already exists.\n");
+            else
+                hashtable.Add(105, "Edward");
+            
+            string? name = (string?) hashtable[102];
+
+            foreach (DictionaryEntry item in hashtable)
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            
+            hashtable.Remove(103);
+            Console.WriteLine($"\nTotal count after deletion: {hashtable.Count}.");
+        }
+    }
+}
---
+++ Exercises/Day24Exercise/HighScoreLeaderboard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace HighScoreLeaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, string> leaderboard = [];
+            leaderboard.Add(55.42, "SwiftRacer");
+            leaderboard.Add(52.10, "SpeedDemon");
+            leaderboard.Add(58.91, "SteadyEddie");
+            leaderboard.Add(51.05, "TurboTom");
+
+            foreach (var item in leaderboard)
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            
+            Console.WriteLine($"\nGold Medalist: {leaderboard.First().Value}");
+
+            leaderboard.Remove(58.91);
+            leaderboard.Add(54.00, "SteadyEddie");
+        }
+    }
+}
