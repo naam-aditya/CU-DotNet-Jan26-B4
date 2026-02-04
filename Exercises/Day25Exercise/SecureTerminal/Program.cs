7e7d77f0f1b4ece7fc406eab2403cd9596cb67fd
aditya_bhardwaj <973adityabhardwaj@gmail.com>
2026-02-04 14:23:52 +0530

    Added Day 25 Exercise - Secure Terminal

---
+++ Exercises/Day25Exercise/SecureTerminal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace SecureTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder pincode = new();
+            do
+            {
+                var pin = Console.ReadKey(true);
+                if (!char.IsBetween(pin.KeyChar, '0', '9'))
+                    continue;
+                
+                Console.Write("*");
+                pincode.Append(pin.KeyChar);
+            }
+            while (pincode.Length != 4);
+
+            Console.WriteLine($"\nPIN: {pincode}");
+        }
+    }
+}
